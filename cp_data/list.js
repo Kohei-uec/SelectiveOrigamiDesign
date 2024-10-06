4f4ef95edb74876b6592fa2ba2fc22e913daa26a
Kohei-uec <y2110652@edu.cc.uec.ac.jp>
2024-10-06 16:42:20 +0900

    add: cp

--- cp_data/list.js
+++ cp_data/list.js
@@ -1,5 +1,5 @@
 export const list = {
-    top: ['f2.cp', 'f3.cp'],
-    bottom: ['f2.cp', 'f3.cp'],
-    side: ['f2.cp', 'f3.cp'],
+    top: ['f2.cp', 'f3.cp', 'f4.cp'],
+    bottom: ['f2.cp', 'f3.cp', 'f4.cp'],
+    side: ['f2.cp', 'f3.cp', 'f4.cp'],
 };
